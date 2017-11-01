c61a5c837834e251e9de71c3a91631d117b3b2d3
hborcher <hborcher@illinois.edu>
2017-11-01 13:47:33 -0500

    include entry point for pytest

--- imgvalidator/__main__.py
+++ imgvalidator/__main__.py
@@ -1,8 +1,16 @@
 from imgvalidator import cli
-
+import sys
 
 def main():
-    cli.main()
+    if len(sys.argv) > 1 and sys.argv[1] == "--pytest":
+        try:
+            import pytest  # type: ignore
+            sys.exit(pytest.main(sys.argv[2:]))
+        except ImportError as e:
+            print("Unable to run tests. Reason {}".format(e), file=sys.stderr)
+            sys.exit(1)
+    else:
+        cli.main()
 
 
 if __name__ == '__main__':
