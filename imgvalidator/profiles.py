21030fc5f6b27b28679785a46368143cd24986ec
hborcher <hborcher@illinois.edu>
2017-11-15 16:52:20 -0600

    added DSPreservationJpeg2000 profile

--- imgvalidator/profiles.py
+++ imgvalidator/profiles.py
@@ -121,3 +121,63 @@ class DSPreservationTiff(validation_profile.AbsProfileBuilder):
 
     def set_file_extension(self):
         self._profile.file_extension = ".tif"
+
+class DSPreservationJpeg2000(validation_profile.AbsProfileBuilder):
+
+    def set_embedded_metadata_profile(self):
+        self._profile.embedded_metadata_profile = {
+            "DPI: Horizontal": validation_profile.ValidationPair(
+                field="Exif.Image.XResolution",
+                expected_value="600/1"
+            ),
+            "DPI: Vertical": validation_profile.ValidationPair(
+                field="Exif.Image.YResolution",
+                expected_value="600/1"
+            ),
+            "Bit depth": validation_profile.ValidationPair(
+                field="Exif.Image.BitsPerSample",
+                expected_value="16 16 16"
+            ),
+            "Creator": validation_profile.ValidationPair(
+                field="Xmp.dc.creator",
+                expected_value="University of Illinois Library"
+            ),
+            "Address": validation_profile.ValidationPair(
+                field="Xmp.iptc.CreatorContactInfo/Iptc4xmpCore:CiAdrExtadr",
+                expected_value="1408 W. Gregory Drive"
+            ),
+            "City": validation_profile.ValidationPair(
+                field="Xmp.iptc.CreatorContactInfo/Iptc4xmpCore:CiAdrCity",
+                expected_value="Urbana"
+            ),
+            "State": validation_profile.ValidationPair(
+                field="Xmp.iptc.CreatorContactInfo/Iptc4xmpCore:CiAdrRegion",
+                expected_value="Illinois"
+            ),
+            "Zip code": validation_profile.ValidationPair(
+                field="Xmp.iptc.CreatorContactInfo/Iptc4xmpCore:CiAdrPcode",
+                expected_value="61801"
+            ),
+            "Country": validation_profile.ValidationPair(
+                field="Xmp.iptc.CreatorContactInfo/Iptc4xmpCore:CiAdrCtry",
+                expected_value="United States"
+            ),
+            "Phone number": validation_profile.ValidationPair(
+                field="Xmp.iptc.CreatorContactInfo/Iptc4xmpCore:CiTelWork",
+                expected_value="+1(217)2442062"
+            ),
+            "Email": validation_profile.ValidationPair(
+                field="Xmp.iptc.CreatorContactInfo/Iptc4xmpCore:CiEmailWork",
+                expected_value="digidcc@library.illinois.edu"
+            ),
+            "Website": validation_profile.ValidationPair(
+                field="Xmp.iptc.CreatorContactInfo/Iptc4xmpCore:CiUrlWork",
+                expected_value="http://www.library.illinois.edu"
+            ),
+        }
+
+    def set_icc_profile_type(self):
+        self._profile.icc_profile_type = validation_profile.IccProfileType.SRGB
+
+    def set_file_extension(self):
+        self._profile.file_extension = ".jp2"
