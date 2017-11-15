682b4bffc1c3df2d6f897d4bc93becc86d70058f
hborcher <hborcher@illinois.edu>
2017-11-15 16:51:46 -0600

    removed metadata from __version__

--- imgvalidator/__init__.py
+++ imgvalidator/__init__.py
@@ -1,5 +1,3 @@
-from .__version__ import __title__ , __version__ , __url__ , __author__ , __author_email__
-from .__version__ import __maintainer__ , __maintainer_email__ , __description__ , FULL_TITLE
 from . import profiles, validation_profile
 from .utils import get_profiles
 from .validation_profile import get_profile_builder
