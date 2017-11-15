0e5dacc7e815bfba31746c67e2bcc2d4d5ea298f
hborcher <hborcher@illinois.edu>
2017-11-15 17:08:25 -0600

    removed

--- imgvalidator/__version__.py
+++
@@ -1,9 +0,0 @@
-__title__ = 'imgvalidator'
-__version__ = '0.0.1'
-__url__ = 'http://http://www.library.illinois.edu/dccdocs/imgvalidator'
-__author__ = 'University of Illinois at Urbana Champaign'
-__author_email__ = 'hborcher@illinois.edu'
-__maintainer__ = 'Henry Borchers'
-__maintainer_email__ =  'hborcher@illinois.edu'
-__description__ = "Validate single image or a directory of images"
-FULL_TITLE = "imgvalidator"
